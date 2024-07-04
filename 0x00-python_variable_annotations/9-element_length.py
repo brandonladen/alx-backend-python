bb24a31a658e2a38679918d9e2e39ae4f72a3186
brandonladen <brandonladen486@gmail.com>
2024-07-04 16:50:35 +0000

    iterable object

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env
+'''
+Model for duck type object
+'''
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''returns tuple in a list'''
+    return [(i, len(i)) for i in lst]
