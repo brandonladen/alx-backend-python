c091dea9e4cf74eb27d9a3782677c5bc7807891d
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-07-04 17:46:25 +0300

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
+'''
+A module
+'''
 
 def add(a: float, b: float) -> float:
-    "This function takes two floats args and return their sum"
+    '''This function takes two floats args and return their sum'''
     return a + b
