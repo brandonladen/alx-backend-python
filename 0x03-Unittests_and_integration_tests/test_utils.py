d651743b1a9eb0f9fdc66ca175a83c855106aaf0
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-07-30 05:53:15 +0300

    Update test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+'''
+Write the unit test for utils.access_nested_map.
+Create a TestAccessNestedMap class
+that inherits from unittest.TestCase.
+Implement the TestAccessNestedMap.test_access_nested_map
+method to test that the method returns what it is supposed to.
+Decorate the method with @parameterized.expand
+to test the function for following inputs
+'''
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for testing Nested Map function """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected_output):
+        """ Test method return output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """ Test method raises correct exception """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(wrong_output, e.exception)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        with patch('requests.get', return_value=mock_response):
+            real_response = get_json(test_url)
+            self.assertEqual(real_response, test_payload)
+            mock_response.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as patched:
+            test_class = TestClass()
+            real_return = test_class.a_property
+            real_return = test_class.a_property
+
+            self.assertEqual(real_return, 42)
+            patched.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
